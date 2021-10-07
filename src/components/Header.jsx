a89409a358eba6b2cd72945fb02e6db4e6607dcf
Juniormzy <72698985+Juniormzy@users.noreply.github.com>
2021-10-07 14:52:18 +0300

    Background Image changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
-import image1 from '../components/images/image2.jpg';
+import video1 from '../components/images/background.mp4';
 import { Link } from 'react-scroll';
 
 function Header() {
     return (
         <div className='header'>
             <Navbar/>
-           <img src={image1} alt='logo' className='image1'/>
-           <div className='header-text'>
+           <video autoPlay loop muted className='image1'>
+               <source src={video1} type="video/mp4"></source>
+            </video>           <div className='header-text'>
            <h1 className='header-h1'>Save the Children</h1>
            <h3 className='header-h3'> When a child gets access to good food, it can change just about everything<br />Every little things does matters, Donate now and save a life</h3>
            <div className='header-btn'>
