5ad051fd45e63488a09453fa7bbe0d836053c50b
Juniormzy <72698985+Juniormzy@users.noreply.github.com>
2021-10-27 17:42:20 +0300

    ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar';
-import video1 from '../components/images/background.mp4';
+import video1 from '../components/images/background3.mp4';
 import { Link } from 'react-scroll';
 
 function Header() {
     return (
         <div className='header'>
-            <Navbar/>
            <video autoPlay loop muted className='image1'>
                <source src={video1} type="video/mp4"></source>
             </video>           <div className='header-text'>
-           <h1 className='header-h1'>Save the Children</h1>
-           <h3 className='header-h3'> When a child gets access to good food, it can change just about everything<br />Every little things does matters, Donate now and save a life</h3>
+           <h1 className='header-h1'>Welcome To Greece</h1>
+           <h3 className='header-h3'> If you are looking for travel inspiration, visitgreece.gr has everything you need to know.<br/> Travel ideas, Events, guides, what to do, where to go in Greece</h3>
            <div className='header-btn'>
-           <a href='#' className='header-button'>Learn More</a>
-           <a href='#' className='header-button2'> Donate Now</a>
+           <a  href='#' className='header-button'>Main Land </a>
+           <a href='#' className='header-button2'>Island</a>
            </div>
            </div>
         </div>
